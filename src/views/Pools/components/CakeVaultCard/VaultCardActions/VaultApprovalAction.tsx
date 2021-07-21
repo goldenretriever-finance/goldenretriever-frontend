5cef458558fc5dc752ead48c0bd893f38fbeb33d
goldenretriever-finance <develop@goldenretriever-finance.com>
2021-07-21 19:31:59 +0900

    Cannot find name 'styled'.戻した

--- src/views/Pools/components/CakeVaultCard/VaultCardActions/VaultApprovalAction.tsx
+++ src/views/Pools/components/CakeVaultCard/VaultCardActions/VaultApprovalAction.tsx
@@ -8,46 +8,13 @@ interface ApprovalActionProps {
   isLoading?: boolean
 }
 
-const EnableWrapper = styled.button`
-    position: relative;
-    width: auto;
-    background-color: rgba(0, 0, 0, 0.13);
-    box-shadow: 0px 4px 4px 0 rgba(0, 0, 0, 0.25);
-    border-radius: 6px;
-    overflow: hidden;
-    border-left: 2px solid #0947e7;
-    border-right: 2px solid #cf00f0;
-    border-image: initial;
-    font-size: 14px;
-    letter-spacing: 0;
-    &::before {
-      content: '';
-      display: block;
-      width: 100%;
-      height: 2px;
-      position: absolute;
-      top: 0;
-      background: linear-gradient(to right, #0947e7 0%, #cf00f0 100%);
-    }
-    &::after {
-      content: '';
-      display: block;
-      width: 100%;
-      height: 2px;
-      position: absolute;
-      bottom: 0;
-      background: linear-gradient(to right, #0947e7 0%, #cf00f0 100%);
-    }
-`
-
-
 const VaultApprovalAction: React.FC<ApprovalActionProps> = ({ isLoading = false, setLastUpdated }) => {
   const { t } = useTranslation()
 
   const { handleApprove, requestedApproval } = useVaultApprove(setLastUpdated)
 
   return (
-    <EnableWrapper>
+    <>
       {isLoading ? (
         <Skeleton width="100%" height="52px" />
       ) : (
@@ -61,7 +28,7 @@ const VaultApprovalAction: React.FC<ApprovalActionProps> = ({ isLoading = false,
           {t('Enable')}
         </Button>
       )}
-    </EnableWrapper>
+    </>
   )
 }
 
