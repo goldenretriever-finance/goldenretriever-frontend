8c11b7e5223900fed69ed7adeafa94316fa500b1
goldenretriever-frontend <develop@goldenretriever-finance.com>
2021-08-25 22:34:33 +0900

    fix 型エラー及びパッケージの互換性問題を解決

--- src/state/index.ts
+++ src/state/index.ts
@@ -27,6 +27,6 @@ const store = configureStore({
  * @see https://redux-toolkit.js.org/usage/usage-with-typescript#getting-the-dispatch-type
  */
 export type AppDispatch = typeof store.dispatch
-export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppDispatch = () => useDispatch<any>()
 
 export default store
